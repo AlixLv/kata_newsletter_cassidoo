f9cbb6c13b0711074678eddf8fb962c11edcf6b4
Alix Levé <alix.leve@gmail.com>
2025-01-06 10:45:54 +0100

    add:check argument in function

--- newYearsDay/newYear.js
+++ newYearsDay/newYear.js
@@ -10,21 +10,29 @@ const weekDays = {
     0: "Sunday",
 };
 
+let pattern = /^[\0-9]{4}$/;
+
 
 function newYearsDay(year){
-    let searchDate = new Date(`${year}-01-01`);
-    let searchDay = searchDate.getDay()
+    if (!(Number.isInteger(year) && pattern.test(year))){
+        throw new RangeError(`${year} is not in the format yyyy. Please enter a four-digit integer.`)
+    }
+    else {
+        let searchDate = new Date(`${year}-01-01`);
+        let searchDay = searchDate.getDay()
+    
+        let days = Object.keys(weekDays);
+        let newDay;
+    
+        days.forEach((day) => {
+            if(searchDay == day){
+                newDay = weekDays[day]
+            } 
+        });
+        return newDay
+    }
 
-    let days = Object.keys(weekDays);
-    let newDay;
-
-    days.forEach((day) => {
-        if(searchDay == day){
-            newDay = weekDays[day]
-        } 
-    });
-    return newDay
 };
         
-let day = newYearsDay(2024);
+let day = newYearsDay(2020);
 console.log(day)
