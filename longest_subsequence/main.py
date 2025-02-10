1051c0d5f22220c490290d3ea69a893c8c5a7c63
Alix Levé <alix.leve@gmail.com>
2025-02-10 16:44:13 +0100

    add: test for longest_subsequence kata

--- longest_subsequence/main.py
+++ longest_subsequence/main.py
@@ -12,22 +12,25 @@
 def get_longest_sub(lst):
     len_lst = len(lst)
     
-    for sub in range(len_lst - 1):
-        current_len_sub = len(lst[sub])
-        print("🥕 ", lst[sub], "len: ", current_len_sub)
-               
-        compared_list = lst[sub + 1]
-        compared_len_sub = len(compared_list)
-        print("🥒 ", compared_list, "len: ", compared_len_sub)
+    try:
+        for sub in range(len_lst - 1):
+            current_len_sub = len(lst[sub])
+            print("🥕 ", lst[sub], "len: ", current_len_sub)
+                
+            compared_list = lst[sub + 1]
+            compared_len_sub = len(compared_list)
+            print("🥒 ", compared_list, "len: ", compared_len_sub)
+                
+            if compared_len_sub > current_len_sub:
+                longest_sub = compared_len_sub
+                print("🐙 ", longest_sub)
+            else:  
+                longest_sub = current_len_sub
+                print("🪼 ", longest_sub)     
             
-        if compared_len_sub > current_len_sub:
-            longest_sub = compared_len_sub
-            print("🐙 ", longest_sub)
-        else:  
-            longest_sub = current_len_sub
-            print("🪼 ", longest_sub)     
-        
-    return longest_sub
+        return longest_sub
+    except:
+        print("An error occured")
     
 
 def longest_subsequence(list):
@@ -35,53 +38,41 @@ def longest_subsequence(list):
     subsequence = []
     len_list = len(list)
 
-    for i in range(len_list - 1):
-        print("🌼 ", list[i])
-        if list[i] + 1 == list[i + 1] or list[i] - 1 == list[i + 1]:
-            print("🍁 ", list[i + 1])
-            if list[i] not in subsequence:
-                subsequence.append(list[i])
-            subsequence.append(list[i + 1])
-            print("🍋 ", subsequence)
-        else:
-            list_subsequences.append(subsequence)
-            print("🥬 ", list_subsequences)   
-            subsequence = []
+    try:
+        for i in range(len_list - 1):
+            print("🌼 ", list[i])
+            if list[i] + 1 == list[i + 1] or list[i] - 1 == list[i + 1]:
+                print("🍁 ", list[i + 1])
+                if list[i] not in subsequence:
+                    subsequence.append(list[i])
+                subsequence.append(list[i + 1])
+                print("🍋 ", subsequence)
+            else:
+                list_subsequences.append(subsequence)
+                print("🥬 ", list_subsequences)   
+                subsequence = []
     
     
-    len_list_subsequences = len(list_subsequences)
-    print("🐸 ", subsequence)
-    
-    if len_list_subsequences == 0:
-        print("🦁 ", len(subsequence))
-        return len(subsequence)
-    
-    else:
-        longest_sub =  get_longest_sub(list_subsequences)
-        return longest_sub
-        # for sub in range(len_list_subsequences - 1):
-        #     current_len_sub = len(list_subsequences[sub])
-        #     print("🥕 ", list_subsequences[sub], "len: ", current_len_sub)
-            
-            
-        #     compared_list = list_subsequences[sub + 1]
-        #     compared_len_sub = len(compared_list)
-        #     print("🥒 ", compared_list, "len: ", compared_len_sub)
-            
-        #     if compared_len_sub > current_len_sub:
-        #         longest_sub = compared_len_sub
-        #         print("🐙 ", longest_sub)
-        #     else:  
-        #         longest_sub = current_len_sub
-        #         print("🪼 ", longest_sub)     
+        len_list_subsequences = len(list_subsequences)
+        print("🐸 ", len_list_subsequences)
         
-        # return longest_sub
+        if len_list_subsequences == 0:
+            print("🦁 ", len(subsequence))
+            return len(subsequence)
+        
+        else:
+            longest_sub =  get_longest_sub(list_subsequences)
+            return longest_sub
+
+    except:
+        print("An error occured")
 
 
 def main():
-    length_subsequence =  longest_subsequence([10,11,7,8,9,12])
-    print(length_subsequence)
-
+    #length_subsequence =  longest_subsequence([10,11,7,8,9,12])
+    #print(length_subsequence)
+    res = get_longest_sub([])
+    print("🐼 ", res)
 
 
 main()    
--- longest_subsequence/test_main.py
+++ longest_subsequence/test_main.py
@@ -1,8 +1,55 @@
 # A CHECKER
-# longest_subsequence() prend bien une liste en paramètre
-# longest_subsequence() prend bien une liste  d'int en paramètre
-# longest_subsequence() retourne bien un int
-
 # get_longest_sub() prend bien une liste de listes en paramètre
 # get_longest_sub() prend bien une liste de listes d'int en paramètre
 # get_longest_sub()  retourne bien un int
+
+import unittest
+from main import longest_subsequence, get_longest_sub
+
+
+class TestLongestSubsequence(unittest.TestCase):
+    def test_list_arg(self):
+        lst = [10,11,7,8,9,12]
+        expected = 3
+        result = longest_subsequence(lst)
+        self.assertEqual(result, expected)
+    
+    def test_empty_list_arg(self):
+        lst = []
+        expected = 0
+        result = longest_subsequence(lst)
+        self.assertEqual(result, expected)
+    
+    def test_arg_in_list(self):
+        lst = ["toto", 11,7,8,9,12]
+        expected = "An error occured"
+        result = longest_subsequence(lst)
+        self.assertRaises(TypeError, "'str' object is not callable")
+
+    def test_type_return_value(self):
+        lst = [4,2,3,1,5]
+        result = longest_subsequence(lst)
+        self.assertIsInstance(result, int)
+
+
+class TestGetLongestSub(unittest.TestCase):
+    def test_list_arg(self):
+        lst_of_lst =  [[10, 11], [7, 8, 9]]
+        expected = 3
+        result = get_longest_sub(lst_of_lst)
+        self.assertEqual(result, expected)
+
+    def test_empty_list_arg(self):
+        lst = []
+        expected = "An error occured"
+        result = get_longest_sub(lst)
+        self.assertRaises(TypeError, "'str' object is not callable")
+     
+    def test_type_return_value(self):
+        lst = [[10, 11], [7, 8, 9]]
+        result = get_longest_sub(lst)
+        self.assertIsInstance(result, int)    
+         
+        
+if __name__ == '__main__':
+    unittest.main()           
